25591d2f6d5b29e5b94b0e3e320b428521d994a2
fatmabadawy <143001487+fatmabadawy@users.noreply.github.com>
2024-04-29 01:36:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function function1() {
+    alert("Added to cart");
+}
+
+function function2(){
+   
+    
+    alert("Account made successfully.");
+    
+}
+
+function show() {
+    const sidebar = document.querySelector('.sidebar')  //returns the first child element that matches a specified css selector of an element
+    sidebar.style.display = 'flex'; 
+}
+function hide() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'; 
+}
+
+function signup() {
+    window.location.href="http://127.0.0.1:5500/theme2/form.html";
+}
+function home() {
+    w;indow.location.href="http://127.0.0.1:5500/theme2/home3.html";
+}
+function login() {
+    w;indow.location.href="";
+}
+function complains() {
+    w;indow.location.href="";
+}

8ddc1298128c5deae3ebc530f703379a7cb49fd0
fatmabadawy <143001487+fatmabadawy@users.noreply.github.com>
2024-04-28 16:03:12 +0300

    Add files via upload

---
+++ html project/project 1/js/scripts.js
@@ -0,0 +1,32 @@
+function myFunction() {
+    alert("Added to cart");
+  }
+
+
+  function login() {
+    alert("you are successfully logged in");
+  } 
+  
+  function buy() {
+    var x=confirm("Do you want to buy this?")
+    if(x){
+        text="Accepted";
+    }
+
+    else{
+        text="Canceled";
+    }
+    alert(text);
+  }
+function footer()
+{
+  window.location.href="file:///C:/html/it%20project%20theme1.html#footer theme1";
+}
+function home()
+{
+  window.location.href="file:///C:/html/it%20project%20theme1.html";
+}
+function feedback()
+{
+  window.location.href="file:///C:/html/feedback%20theme1.html";
+}

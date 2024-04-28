8ed38ded9dbc8e016354ab15ea62d16f8014ba97
fatmabadawy <143001487+fatmabadawy@users.noreply.github.com>
2024-04-28 19:32:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,58 @@ function hide() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none' 
 }
+/*search part*/
+let availablekeywords1 = [
+    'clothes',
+    'shoes',
+    'skin care',
+    'supermarket',
+    'electronics devices',
+    'electricity',
+    'medical devices',
+    'books',
+];
+const resultbox=document.querySelector(".result-box");
+const inputbox=document.getElementById("input-box");
+
+inputbox.onkeyup = function()
+{
+    let result=[];
+    let input=inputbox.value;
+    if(input.length){
+        result=availablekeywords1.filter((keyword)=>{
+            return   keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+   display(result);
+    if(!result.length)
+    {
+        resultbox.innerHTML='';
+    }
+}
+function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick=selectinput(this)>"+list+"</li>";
+    });
+    resultbox.innerHTML="<ul>"+ content.join('') +"</ul>";
+}
+
+function selectinput(list){
+    inputbox.value=list.innerHTML;
+    resultbox.innerHTML='';
+}
+function goButton()
+{
+    let search =inputbox.value.toLowerCase() ; 
+    if(search.includes('clothes')){
+    window.location.href ="file:///C:/clothes%20part/men%20part.html";
+    }
+    else if(search.includes('skin care')){
+        window.location.href ="file:///C:/html/html%20project/project%201/index.html";
+}
+}
+
+
+
+

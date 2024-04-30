0aeeec8e2be998ffdf2e1f56114b22560c509c29
fatmabadawy <143001487+fatmabadawy@users.noreply.github.com>
2024-04-30 18:52:53 +0300

    AliaaMohamed - electronics

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-function function1() {
-    alert("Added to cart");
-}
+<script>
 
-function function2(){
-   
-    
-    alert("Account made successfully.");
-    
-}
+    var productImages = document.querySelectorAll('.product img');
+    productImages.forEach(function(image) {
+        image.addEventListener('mouseover', function() {
+            this.style.transform = 'scale(1.1)';
+        });
+        image.addEventListener('mouseout', function() {
+            this.style.transform = 'scale(1)';
+        });
+    });
 
-function show() {
-    const sidebar = document.querySelector('.sidebar')  //returns the first child element that matches a specified css selector of an element
-    sidebar.style.display = 'flex'; 
-}
-function hide() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'; 
-}
+    function addToCart() {
+        alert("Product added to cart!");
+    }
 
-function signup() {
-    window.location.href="http://127.0.0.1:5500/theme2/form.html";
-}
-function home() {
-    w;indow.location.href="http://127.0.0.1:5500/theme2/home3.html";
-}
-function login() {
-    w;indow.location.href="";
-}
-function complains() {
-    w;indow.location.href="";
-} 
+    function changeButtonColor(button) {
+        button.style.backgroundColor = "lightblue";
+    }
+
+    function resetButtonColor(button) {
+        button.style.backgroundColor = ""; // Reset to default color
+    }
+</script>
